6bc7afdb402c9ab6b663dd17559f20e1d02a12f1
unknown <shindekuldeep1404@gmail.com>
2026-02-03 18:58:15 +0530

    Fix API baseURL using env variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7000/api", // Change as per your backend port
+  baseURL: import.meta.env.VITE_API_URL + "/api", // Change as per your backend port
   headers: {
     "Content-Type": "application/json",
   },
